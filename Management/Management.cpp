e02ab55447d36fb6d87188a3531f88b3f914f823
Shuyu Xing <ning1yan2jie3@163.com>
2022-08-18 23:13:31 +0800

    加入中间件

--- Management/Management.cpp
+++ Management/Management.cpp
@@ -2,15 +2,28 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include "yaml-cpp/yaml.h"
 using namespace std;
+using namespace YAML;
 
 int main()
 {
     std::cout << "Config go-cqhttp\n";
     int QQ;
     string password;
+    cout << "QQ:\n";
     cin >> QQ;
+    cout << "password:\n";
+    cin >> password;
+    Node config = LoadFile("plugins\\X-Robot\\go-cqhttp\\config.yml");
+    config["account"]["uin"] = QQ;
+    config["account"]["password"] = password;
+    ofstream fout;
+    fout.open("plugins\\X-Robot\\go-cqhttp\\config.yml");
+
 }
+
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
