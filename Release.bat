caea13bdf72e7269deda2a8ca117aa7e34d5e357
XingShuyu <Ning1Yan2Jie3@163.com>
2022-12-15 15:48:53 +0800

    wu

---
+++ Release.bat
@@ -0,0 +1,18 @@
+echo "Making Folders..."
+mkdir ReleasePack
+mkdir ReleasePack\plugins
+mkdir ReleasePack\plugins\X-Robot
+mkdir ReleasePack\plugins\X-Robot\go-cqhttp
+
+echo "Copy Files..."
+
+copy .\x64\Release\Manager.exe .\ReleasePack\
+copy .\x64\Release\X-Robot.dll .\ReleasePack\plugins\
+copy .\x64\Release\*.pdb .\ReleasePack\plugins\
+copy .\Management\*.bat .\ReleasePack\
+copy .\Management\yaml-cpp\Lib\yaml-cpp.dll .\ReleasePack\
+copy .\Management\BDS-Deamon\BDS-Deamon.cmd .\ReleasePack\
+copy .\Json\*.json .\ReleasePack\plugins\X-Robot\
+copy .\go-cqhttp\* .\ReleasePack\plugins\X-Robot\go-cqhttp\
+powershell Compress-Archive .\ReleasePack\* .\X-Robot.zip
+powershell rm .\ReleasePack\
