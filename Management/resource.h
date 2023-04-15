6375d5c432d7e7d54fd3ea7f777757902d14ae6e
星纾妤 <ning1yan2jie3@163.com>
2023-04-15 08:28:23 +0800

    增加图标(虽然没什么用)

---
+++ Management/resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Manager.rc 使用
+//
+#define IDI_ICON1                       101
+#define IDB_BITMAP1                     103
+#define IDI_ICON2                       104
+#define IDB_BITMAP2                     105
+#define IDI_ICON3                       106
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
